e45001a767fde8000dab33528145733ce78b6736
KedaiTube84 <97569857+KedaiTube84@users.noreply.github.com>
2022-02-10 11:28:02 +0800

    Update __main__.py

--- Atrocious_Mirror_Bot/__main__.py
+++ Atrocious_Mirror_Bot/__main__.py
@@ -44,9 +44,9 @@ def stats(update, context):
         
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/smexynos7870")
-    buttons.buildbutton("Cloud Drive Group", "https://t.me/joinchat/WKZqyWNHpLViMmI1")
-    buttons.buildbutton("Support Group", "https://t.me/Atrocious_Bot_Support")
+    buttons.buildbutton("Owner", "https://t.me/Collection_MovieTerbaik")
+    buttons.buildbutton("Cloud Drive Group", "https://t.me/RequestMovie84")
+    buttons.buildbutton("Subtitle", "https://subscene.com/u/1271292")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -55,7 +55,7 @@ I can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 For any question join [Atrocious Bot Support](t.me/Atrocious_Bot_Support)
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/16165db70a6d8c866eeed.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/797e2fd172b1b65bad264.jpg", start_string, parse_mode=ParseMode.MARKDOWN)
         
 def ping(update, context):
         sendMarkup(
@@ -157,9 +157,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Atrocious Mirror Help',
-        author_name='AL-Noman',
-        author_url='https://t.me/smexynos7870',
+        title='CMT Mirror Help',
+        author_name='Collection Movie Terbaru',
+        author_url='https://t.me/Collection_MovieTerbaik',
         html_content=help_string_telegraph,
     )["path"]
 
